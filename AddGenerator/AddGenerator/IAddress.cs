33b6dcf7c0c00f3b6ba81cc76e8f153e3edb4ea4
LuciaZangheratti <94352248+LuciaZangheratti@users.noreply.github.com>
2023-05-04 23:31:02 +0200

    esercizio AddGenerator

---
+++ AddGenerator/AddGenerator/IAddress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddGenerator
+{
+    interface IAddress
+    {
+        string generateIPv4();
+        string generateSubnet();
+    }
+}
---
+++ AddGenerator/AddGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AddGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressGenerator risultato;
+            Console.WriteLine("Inserisci l'indirizzo ip ");
+
+            string risposta = Console.ReadLine();
+            risultato = new AddressGenerator(risposta);
+
+            Console.WriteLine(risultato.generateIPv4());
+            Console.WriteLine(risultato.generateSubnet());
+
+            Console.ReadLine();
+        }
+    }
+}
